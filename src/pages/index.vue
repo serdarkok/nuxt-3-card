f4bdc71f738f96a627280550171c57b465197e83
serdarkok <kokserdal@gmail.com>
2024-01-30 17:09:07 +0300

    update homepage meta

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,6 +6,12 @@
 </template>
 
 <script setup lang="ts">
+  useHead({
+    title: 'Nuxt 3 Card Demo',
+    meta: [
+      { name: 'description', content: 'Nuxt 3 Card demo description' }
+    ]
+  });
   import Card from '@/components/shared/cards/Card.vue';
   import Button from '@/components/shared/Button.vue';
 </script>
